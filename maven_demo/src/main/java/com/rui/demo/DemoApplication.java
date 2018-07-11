74563dfe1285eb3172d706016022f0a8e1ef9dac
liangrui <liangrui_1988@126.com>
2018-07-11 20:40:56 +0800

    init

--- maven_demo/src/main/java/com/rui/demo/DemoApplication.java
+++ maven_demo/src/main/java/com/rui/demo/DemoApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 1：start.spring.io 中构建
+ * <p>
  * 2：maven中构建
+ * <p>
  * 运行: mvn spring-boot:run ,java -jar xx.jar,
+ * <p>
+ * 修改端口-Dserver.port=8009
+ * <p>
  */
 @SpringBootApplication
 public class DemoApplication {
